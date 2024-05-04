508a48be30bc2744ece4f1045b1425a994762aae
inassar86 <168650138+inassar86@users.noreply.github.com>
2024-05-04 17:45:15 +0300

    lecture3

---
+++ lect3.py
@@ -0,0 +1,36 @@
+#print('hello world')
+#dic1={'name':'Ibrahim','age':37,'address':'Hubras','phonenumber':795999999,'email':'ibrahim@gmail.com'}
+#print(dic1)
+#print(dic1['email'])
+#print(dic1['name'])
+#dic1['age']=38
+#print(dic1['age'])
+name1='ali'
+name2="sami"
+name3="""hasan"""
+name4="""moh
+test
+ali
+ziad
+ahmad"""
+# print(name1)
+# print(name2)
+# name5=("ahmad\nhasan")
+# print(name3)
+# print(name4)
+# print(name5)
+# name6='ali nsjakas'
+# print(name6[3])
+# list1=[1,2,3,4,5,6]
+# print(list1[5])
+
+# list1[5]=8
+# #print(list1[5])
+# #print(list1[3:5])
+# complex1=2+2j
+# print (complex1.real)
+# print (complex1.imag)
+# bool1=0
+# print(bool1)
+set1={1,2,'3',4,5,6}
+print (set1)
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+#print('hello world')
+dic1={'name':'Ibrahim','age':37,'address':'Hubras','phonenumber':795999999,'email':'ibrahim@gmail.com'}
+print(dic1)
+print(dic1['email'])
+print(dic1['name'])
+dic1['age']=38
+print(dic1['age'])
